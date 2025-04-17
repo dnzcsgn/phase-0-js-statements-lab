04e6f7f0ceff278ad55a8d95d7029f9ebb3ee1dc
dnzcsgn <dnzcsgndc@gmail.com>
2025-04-16 17:52:14 -0700

    garden system

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,26 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+//Water control
+if (temperature > 80) {
+    console.log('Watering on');
+}  else { 
+    console.log('Watering off')
+}
+
+
+// Light control
+if (timeOfDay === 'evening') {
+    console.log('Lights on')
+}  else if   (timeOfDay === 'night') {
+    console.log('Lights off')
+}  else { 
+   console.log('Lights off')
+}
+
+//soil control
+while (soilMoisture < 40) {
+    soilMoisture += 5;
+    console.log(soilMoisture);
+}
